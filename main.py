b5b323a8d6a688786852e8520518a9198db30d2b
Abhinav Sharma <abhi.sharma19jan@gmail.com>
2025-02-13 19:04:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
+from turtle import Turtle,Screen
+import time
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
+scores=Scoreboard()
+screen=Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
 
+snake=Snake()
+food=Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on=True
+while game_is_on:
+
+    snake.move_snake()
+    screen.update()
+    time.sleep(0.1)
+
+    if food.distance(snake.head)<15:
+        food.refresh()
+        scores.increase_score()
+        snake.extend()
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280 :
+        game_is_on=False
+        scores.game_over()
+
+    #Detect collision with tail
+    for segment in snake.snake_body[1::-1]:
+        if snake.head.distance(segment)<10:
+            scores.game_over()
+            game_is_on=False
+
+screen.exitonclick()
